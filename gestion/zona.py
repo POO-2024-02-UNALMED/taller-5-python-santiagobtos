594c50594a8bdb0e4a5c7126f554143aad8b927b
santiagobtos <sbarrientosm@unal.edu.co>
2024-12-14 11:53:24 -0500

    as

---
+++ gestion/zona.py
@@ -0,0 +1,20 @@
+class Zona():
+    def __init__(self,nombre,zoo):
+        self._nombre=nombre
+        self._zoo=zoo
+        self._animales=[]
+    def cantidadAnimales(self):
+        return len(self._animales)
+    def agregarAnimales(self,animal):
+        self._animales.append(animal)
+    def setNombre(self, nuevo):
+        self.nombre = nuevo
+
+    def getNombre(self):
+        return self.nombre
+
+    def setZoo(self, zo):
+        self.zoo = zo
+
+    def getZoo(self):
+        return self.zoo
---
+++ zooAnimales/ave.py
@@ -0,0 +1,27 @@
+from zooAnimales.animal import Animal
+class Ave(Animal):
+    listado=[]
+    halcones=0
+    aguilas=0
+    total=0
+    def __init__(self, nombre, edad, habitat, genero, piernas ,zona=None):
+        super().__init__(nombre, edad, habitat, genero, zona)
+        self.colorplumas=piernas
+        Ave.listado.append(self)
+        Ave.total=+1
+    def movimiento(self):
+        return "volar"
+    @classmethod
+    def cantidadAves(cls):
+        contador=cls.aguilas+cls.halcones+cls.total
+        return contador
+    @classmethod
+    def creaHalcon(cls,name,edad,genero):
+        nuevom=Ave(name,edad,"montanas",genero,"cafe glorioso") 
+        cls.halcones+=1
+        return nuevom
+    @classmethod
+    def crearAguila(cls,name,edad,genero):
+        nuevom=Ave(name,edad,"montanas",genero,"blanco y amarillo")   
+        cls.aguilas+=1
+        return nuevom
